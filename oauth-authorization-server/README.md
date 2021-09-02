2e7078b29da04d12f07cfd6f8985ad0dc2d72d25
Mubal <mu.ballikaya@gmail.com>
2021-09-02 18:40:29 +0300

    first commit

---
+++ oauth-authorization-server/README.md
@@ -0,0 +1,5 @@
+### Relevant Articles:
+
+- [Keycloak Embedded in a Spring Boot Application](https://www.baeldung.com/keycloak-embedded-in-spring-boot-app)
+
+Project is modified by murat ballikaya
---
+++ oauth-authorization-server/src/main/java/com/baeldung/auth/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.baeldung.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/book")
+public class UserController {
+}
